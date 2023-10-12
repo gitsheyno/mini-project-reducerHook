6020dee038b1da02b9509862427ff0e040da795f
shayan heidary <hdrydeveloper@gmail.com>
2023-10-12 13:52:02 +0200

    reducer init and first action implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
 
+const reducerFunction = (state, action) => {
+  console.log(action);
+  if (action.type === "OPENACCOUNT") {
+    return {
+      ...state,
+      balance: 500,
+      accountisOpen: true,
+    };
+  }
+  return state;
+};
 function App() {
+  const [state, dispatch] = useReducer(reducerFunction, {
+    balance: 0,
+    loan: 0,
+    activeLoan: false,
+    accountisOpen: false,
+  });
+
+  const openAccountHandler = () => {
+    console.log("hello");
+    dispatch({ type: "OPENACCOUNT" });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>useReducer Bank Account</h1>
+      <p>Balance : {state.balance}</p>
+      <p>Loan : {state.loan}</p>
+      <button onClick={openAccountHandler}>open account</button>
+      <button disabled={!state.accountisOpen}>deposit 100</button>
+      <button disabled={!state.accountisOpen}>withdraw 50</button>
+      <button disabled={!state.accountisOpen}>Request a loan</button>
+      <button disabled={!state.accountisOpen}>pay loan</button>
+      <button disabled={!state.accountisOpen}>Close Account</button>
     </div>
   );
 }
