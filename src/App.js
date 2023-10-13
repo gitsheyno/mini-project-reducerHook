5ff22607263e0acb803af5d3118e9c5b7cfe519c
shayan heidary <hdrydeveloper@gmail.com>
2023-10-13 10:26:51 +0200

    all functionalities completed

--- src/App.js
+++ src/App.js
@@ -5,8 +5,37 @@ const reducerFunction = (state, action) => {
   if (action.type === "OPENACCOUNT") {
     return {
       ...state,
-      balance: 500,
+      balance: +500,
       accountisOpen: true,
+    };
+  } else if (action.type === "DEPOSIT") {
+    return {
+      ...state,
+      balance: state.balance + 100,
+    };
+  } else if (action.type === "WITHDRAW") {
+    return {
+      ...state,
+      balance: state.balance - 50,
+    };
+  } else if (action.type === "REQLOAN") {
+    return {
+      ...state,
+      balance: state.balance + 5000,
+      activeLoan: true,
+      loan: +5000,
+    };
+  } else if (action.type === "PAYLOAN") {
+    return {
+      ...state,
+      balance: state.balance - state.loan,
+      loan: 0,
+      activeLoan: false,
+    };
+  } else if (action.type === "CLOSE") {
+    return {
+      ...state,
+      accountisOpen: false,
     };
   }
   return state;
@@ -23,17 +52,54 @@ function App() {
     console.log("hello");
     dispatch({ type: "OPENACCOUNT" });
   };
+
+  const despositHandler = () => {
+    dispatch({ type: "DEPOSIT" });
+  };
+
+  const withdrawHandler = () => {
+    dispatch({ type: "WITHDRAW" });
+  };
+
+  const reqLoanHandler = () => {
+    !state.activeLoan && dispatch({ type: "REQLOAN" });
+  };
+
+  const payLoanHandler = () => {
+    state.balance > state.loan && dispatch({ type: "PAYLOAN" });
+  };
+  const closeHandler = () => {
+    !state.loan && !state.balance && dispatch({ type: "CLOSE" });
+  };
   return (
     <div className="App">
       <h1>useReducer Bank Account</h1>
       <p>Balance : {state.balance}</p>
       <p>Loan : {state.loan}</p>
-      <button onClick={openAccountHandler}>open account</button>
-      <button disabled={!state.accountisOpen}>deposit 100</button>
-      <button disabled={!state.accountisOpen}>withdraw 50</button>
-      <button disabled={!state.accountisOpen}>Request a loan</button>
-      <button disabled={!state.accountisOpen}>pay loan</button>
-      <button disabled={!state.accountisOpen}>Close Account</button>
+      <button disabled={state.accountisOpen} onClick={openAccountHandler}>
+        open account
+      </button>
+      <button disabled={!state.accountisOpen} onClick={despositHandler}>
+        deposit 100
+      </button>
+      <button
+        disabled={!state.accountisOpen || state.balance === 0}
+        onClick={withdrawHandler}
+      >
+        withdraw 50
+      </button>
+      <button
+        disabled={!state.accountisOpen || state.activeLoan}
+        onClick={reqLoanHandler}
+      >
+        Request a loan
+      </button>
+      <button disabled={!state.accountisOpen} onClick={payLoanHandler}>
+        pay loan
+      </button>
+      <button disabled={!state.accountisOpen} onClick={closeHandler}>
+        Close Account
+      </button>
     </div>
   );
 }
